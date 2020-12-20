2f6832e49593d0c7340557a51fe434b34f906edf
Nobutaka Shiraki <shiraki.devi@gmail.com>
2020-12-20 12:26:19 +0900

    ブロック破壊のパーティクルを追加

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
 	public int life;
+	// 爆発パーティクル
+	public GameObject explosionParticlePrefab;
 	// GameControllerにアクセスする変数
 	GameController gameController;
 
@@ -18,7 +20,13 @@ public class Block : MonoBehaviour
 		life--;
 		if (life <= 0)
 		{
+			// 爆発パーティクルのオブジェクト生成
+			GameObject explosionObject = Instantiate(explosionParticlePrefab, transform.position, transform.rotation);
+			// 爆発パーティクルを1秒後に消す
+			Destroy(explosionObject, 1f);
+			// GameControllerに通知する
 			gameController.OnDestroyBlock(this);
+			// ブロックを消す
 			Destroy(this.gameObject);
 		}
 	}
